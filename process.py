dea787ba3ee05eed9c85363314e867afc3132d25
Vladimir Ovsyannikov <vladimir.ovsyannikov@evision-software.com>
2017-03-16 10:34:27 +0100

    Parsed exported files

--- process.py
+++ process.py
@@ -21,7 +21,7 @@ PARTNERS = {
 TESTCASE_NAME_PATTERN = re.compile('\w*\d*(?=V\d*)')
 TESTCASE_VERSION_PATTERN = re.compile('(?<=V)\d*')
 EXPORT_VERSION_PATTERN = re.compile("(?<=EX)\d+")
-print('|'.join(PARTNERS.keys()))
+IMPORT_VERSION_PATTERN = re.compile('(?<=IM)\d+')
 VENDOR_PATTERN = re.compile('|'.join(PARTNERS.keys()))
 
 
@@ -35,7 +35,7 @@ if __name__ == '__main__':
     # exported = map()
     imported = glob.glob('./**/*EX[0-9]*-*IM[0-9]*.png', recursive=True)
 
-    columns = ['TestCase', 'VendorFrom', 'TestCase_Version', 'Export_Version']
+    columns = ['TestCase', 'VendorFrom', 'TestCaseVersion', 'ExportVersion']
     exported_df = pd.DataFrame(columns=columns)
 
     for item in exported:
@@ -43,14 +43,27 @@ if __name__ == '__main__':
         test_case = TESTCASE_NAME_PATTERN.findall(name)[0]
         test_case_version = TESTCASE_VERSION_PATTERN.findall(name)[0]
         vendors = VENDOR_PATTERN.findall(name)
-        print(vendors)
         exported_version = EXPORT_VERSION_PATTERN.findall(name)[0]
         exported_df.loc[len(exported_df)] = [test_case, vendors[0], test_case_version, exported_version]
-        print(name, test_case, vendors[0], test_case_version, exported_version)
 
     print(exported_df)
 
-    columns = ['TestCase', 'VendorFrom', 'TestCase_Version', 'Export_Version']
+    columns = ['TestCase', 'VendorFrom', 'TestCaseVersion', 'ExportVersion', 'VendorTo', 'ImportVersion']
+
+    imported_df = pd.DataFrame(columns=columns)
+
+    for item in imported:
+        name = os.path.basename(item)
+        test_case = TESTCASE_NAME_PATTERN.findall(name)[0]
+        test_case_version = TESTCASE_VERSION_PATTERN.findall(name)[0]
+        vendors = VENDOR_PATTERN.findall(name)
+        exported_version = EXPORT_VERSION_PATTERN.findall(name)[0]
+        imported_version = IMPORT_VERSION_PATTERN.findall(name)[0]
+        imported_df.loc[len(imported_df)] = [test_case, vendors[0], test_case_version, exported_version, vendors[1], imported_version]
+
+    print(imported_df)
+
+
 
 
 
